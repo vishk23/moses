ca06fcc9fc984ac49490493639a8d68912ae4f99
Chad Doorley <chad.doorley@bcsbmail.com>
2025-07-29 09:31:47 -0400

    fixed orchestr8 runner script

--- orchestr8/django_app/simplified_run.bat
+++ orchestr8/django_app/simplified_run.bat
@@ -1,10 +1,10 @@
 @echo off
 cd /d %~dp0
 echo Starting Server...
-start "Server" cmd /k "call ../.venv/Scripts/activate && waitress-serve --host=127.0.0.1 --port=8000 django_app.wsgi:application"
+start "Server" cmd /k "call ../../.venv/Scripts/activate && waitress-serve --host=127.0.0.1 --port=8000 django_app.wsgi:application"
 @REM echo Starting Scheduler...
-@REM start "Scheduler" cmd /k "call ../.venv/Scripts/activate && celery -A django_app beat --loglevel=info"
+@REM start "Scheduler" cmd /k "call ../../.venv/Scripts/activate && celery -A django_app beat --loglevel=info"
 echo Starting Worker...
-start "Worker" cmd /k "call ../.venv/Scripts/activate && celery -A django_app worker --pool=solo --loglevel=info"
+start "Worker" cmd /k "call ../../.venv/Scripts/activate && celery -A django_app worker --pool=solo --loglevel=info"
 echo All services started. Feel free to close this window, but leave the others open.
 pause
