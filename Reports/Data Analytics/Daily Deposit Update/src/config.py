e50d751b0ecd3997b0b06b3f7eb592939b0dd509
Chad Doorley <chad.doorley@bcsbmail.com>
2025-07-30 23:47:19 -0400

    Refactor environment variable usage for staging and output directories in config

--- Reports/Data Analytics/Daily Deposit Update/src/config.py
+++ Reports/Data Analytics/Daily Deposit Update/src/config.py
@@ -16,11 +16,14 @@ OWNER = "Chad Doorley"
 # Status
 PROD_READY = True
 
+# Environment
+ENV = os.getenv('REPORT_ENV', 'dev')
+
 # Staging Output Directory (for intermediate/staging files)
-STAGING_OUTPUT_DIR = Path(r"\\00-da1\Home\Share\Data & Analytics Initiatives\Project Management\Data_Analytics\Daily_Deposit_Update\Production\output") if os.getenv('REPORT_ENV', 'dev') == 'prod' else Path(__file__).parent.parent / "output"
+STAGING_OUTPUT_DIR = Path(r"\\00-da1\Home\Share\Data & Analytics Initiatives\Project Management\Data_Analytics\Daily_Deposit_Update\Production\output") if ENV == 'prod' else Path(__file__).parent.parent / "output"
 
 # Drop-in/Final Output Directory (for drop-in file to business)
-OUTPUT_DIR = Path(r"\\00-DA1\Home\Share\Line of Business_Shared Services\Commercial Lending\Deposits\DailyDeposit") if os.getenv('REPORT_ENV', 'dev') == 'prod' else Path(__file__).parent.parent / "output"
+OUTPUT_DIR = Path(r"\\00-DA1\Home\Share\Line of Business_Shared Services\Commercial Lending\Deposits\DailyDeposit") if ENV == 'prod' else Path(__file__).parent.parent / "output"
 
 # Input Directory (if needed)
 INPUT_DIR = Path(__file__).parent.parent / "input"
