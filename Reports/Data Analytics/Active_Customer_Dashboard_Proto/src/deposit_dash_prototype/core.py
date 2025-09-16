af78b5ec02994832d60f1b8c7504baf57b508c29
Chad Doorley <Chad.Doorley@bcsbmail.com>
2025-09-16 17:18:41 -0400

    got pbi loader ready month end and year end balances

--- Reports/Data Analytics/Active_Customer_Dashboard_Proto/src/deposit_dash_prototype/core.py
+++ Reports/Data Analytics/Active_Customer_Dashboard_Proto/src/deposit_dash_prototype/core.py
@@ -36,16 +36,27 @@ def main_pipeline():
     specified_date = datetime(2025, 8, 31)
     month_end_df = cdutils.acct_file_creation.core.query_df_on_date(specified_date)
 
-    # Get month end metrics to append to that
-    # Actually -> ME balance, YTD balance
-    # We should be creating a merged_history_df with acctnbr as primary key, month end balance, and year end balance
-    # this gets left joined to 'df'
+    year_end_df = year_end_df[[
+        'acctnbr',
+        'Net Balance'
+    ]].copy()
+
+    month_end_df = month_end_df[[
+        'acctnbr',
+        'Net Balance'
+    ]].copy()
+
+    merged_history_df = year_end_df.merge(month_end_df, how='outer', on='acctnbr', suffixes=('_prior_year','_prior_month'))
+
+    merged_history_df = merged_history_df.fillna(0)
+
+    df = df.merge(merged_history_df, how='left', on='acctnbr').copy()
+
+
+    return df
 
 # Absent of proper portfolio silver dimension table
 # Separate field for presence of Muni (Y/N)
 # We group by portfolio key and come up with a dimension
 # Example: portfolio key is primary key and then I have a calculated field (agg, probably lambda function)
-
-    return df
-
 
